ed86653fe917430369ab7a4e8cbede3e3e8f9f09
Jeff Crockett <jeffreyneilcrockett@gmail.com>
2017-06-25 04:24:55 -0400

    Added folder.js

---
+++ about_OOP/in_javascript/folder.js
@@ -0,0 +1,13 @@
+class CodeMonkey {
+	constructor(your_name){
+		this.s_name = your_name;
+
+this.makes_a_folder = function(name){
+	var s_folder_name = name;
+	return s_folder_name;
+}
+}
+}
+var jeff = new CodeMonkey('jeff');
+console.log(jeff.makes_a_folder('Sentence'));
+
