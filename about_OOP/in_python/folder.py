acaab335149a1a25a15773af59505305876c5578
Jeffrey Neil Crockett <jeffreyneilcrockett@gmail.com>
2017-06-25 04:36:40 -0400

    Update folder.py

---
+++ about_OOP/in_python/folder.py
@@ -0,0 +1,11 @@
+class CodeMonkey(object):
+  def __init__(self, your_name):
+    self.s_name = your_name
+  def makes_a_folder(self, folder_name):
+    self.s_folder_name = name
+    return self
+ 
+    
+jeff = CodeMonkey('jeff')
+print jeff.s_name
+    
