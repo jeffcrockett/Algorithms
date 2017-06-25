1c5cff26c237304408ca14b74aefd04acc9ea91f
Jeffrey Neil Crockett <jeffreyneilcrockett@gmail.com>
2017-06-25 04:37:10 -0400

    Update folder.py

--- about_OOP/in_python/folder.py
+++ about_OOP/in_python/folder.py
@@ -2,7 +2,7 @@ class CodeMonkey(object):
   def __init__(self, your_name):
     self.s_name = your_name
   def makes_a_folder(self, folder_name):
-    self.s_folder_name = name
+    self.s_folder_name = folder_name
     return self
  
     
